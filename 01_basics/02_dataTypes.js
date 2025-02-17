f2b205c0ee4f371095d3b740e29fa39ebc865a80
GitOfSanchita <missanchita2002@gmail.com>
2025-02-17 15:13:01 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  // treat all js code as new version
+
+// alert( 3 + 3 )    // applicable only for browser   // Here we are using nodejs, not browser
+
+console.log(3 
+    +
+     3)  // code readability should be high
+
+console.log("Sanchita")
+
+
+let name = "Sanchita"   // string datatype
+let age = 18            // number datatype
+let isLoggedIn = false  // boolean datatype
+let state;              // undefined => value is not assigned
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof "vvufujhgfut");
+console.log(typeof age);
+console.log(typeof null);    // type of null is object
+console.log(typeof undefined);  // type => undefined
+
+
