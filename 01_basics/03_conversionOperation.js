9f9d7893360a0ed90a2835540dd328eee8a1f8e1
GitOfSanchita <missanchita2002@gmail.com>
2025-02-26 10:09:51 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,31 @@ let someNumber = 45
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+/////////////   OPERATIONS   //////////////
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+console.log(2**3);  //power operation
+console.log(2/3);   //devide operation
+console.log(2%3);   //Remainder operation
+
+let str1 = "hello"
+let str2 = " sanchita"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 100
+gamecounter++;  // or "++gamecounter"  (prefix & postfix)
+console.log(gamecounter);
