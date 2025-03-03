9d92b9bf0bedd96026420986ae3eefd3ce408d87
GitOfSanchita <missanchita2002@gmail.com>
2025-03-03 04:54:44 +0000

    Numbs and maths

---
+++ 01_basics/06_Numbs_and_Maths.js
@@ -0,0 +1,12 @@
+ // Numbers in javascript
+
+ const score = 400
+ console.log(score);
+ 
+ const balance = new Number(100)  // Object definition
+ console.log(balance);
+ 
+ console.log(balance.toString().length);   // "toString()" method used for number to string conversion.
+ console.log(balance.);
+ 
+ 
