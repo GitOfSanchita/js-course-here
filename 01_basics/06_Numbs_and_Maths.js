6734f78b309a1fd0070006f2961db723a8b560c4
GitOfSanchita <missanchita2002@gmail.com>
2025-04-03 09:21:24 +0000

    numbers and maths

--- 01_basics/06_Numbs_and_Maths.js
+++ 01_basics/06_Numbs_and_Maths.js
@@ -7,6 +7,9 @@
  console.log(balance);
  
  console.log(balance.toString().length);   // "toString()" method used for number to string conversion.
- console.log(balance.);
+ console.log(balance.toFixed(2));
+ 
+ const otherNumber = 123.8966
+ console.log(otherNumber.toPrecision(3));
  
  
