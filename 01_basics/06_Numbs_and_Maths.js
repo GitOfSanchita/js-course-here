911d3a640b62671983d87b539c8a4e4aa18d8dde
GitOfSanchita <missanchita2002@gmail.com>
2025-06-22 06:14:08 +0000

    maths and numbs

--- 01_basics/06_Numbs_and_Maths.js
+++ 01_basics/06_Numbs_and_Maths.js
@@ -17,3 +17,22 @@
  console.log(hundreds.toLocaleString());   //1,000,000 -> US standard
  console.log(hundreds.toLocaleString('en-IN'));   //10,00,000
 
+
+
+//**************************************** Maths ******************************************
+
+console.log(Math);  // object [Math] {}
+console.log(Math.abs(-4));  //4    // abs (absolute) -> converting (-)ve value into a (+)ve value
+console.log(Math.round(4.5));    //5     // 4.2 -> 4,  4.8 -> 5
+console.log(Math.ceil(4.2));    //5
+console.log(Math.floor(4.9));    //4
+console.log(Math.min(4, 3, 6, 8));   //3
+console.log(Math.max(4, 3, 6, 8));   //8
+
+console.log(Math.random());    // give a random value in range of 0 to 1
+console.log((Math.random()*10) + 1);  
+console.log(Math.floor(Math.random()*10) + 1);  // give a lowest round off value
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
