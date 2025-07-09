e32e1f4c75c2eb0b113b410247bbfe2346787d3e
GitOfSanchita <missanchita2002@gmail.com>
2025-07-09 06:46:09 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,10 @@ let a = 300      // value of 'a' is independent for the 'if' function.
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner:", a);    // Inner: 10    => Blog scope
+    console.log("Inner:", a);    // Inner: 10    => Block scope
     
    //var  //c = 30
-}                  // here '{}' is a Blog scope inside 'if'.   Any scope outside 'if' is Global scope.
+}                  // here '{}' is a Block scope inside 'if'.   Any scope outside 'if' is Global scope.
 
 //console.log(a);    // ReferenceError: a is not defined
 //console.log(b);    // ReferenceError: b is not defined
