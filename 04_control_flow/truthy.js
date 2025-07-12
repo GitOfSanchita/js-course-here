6de7dc90d2c33d09bbd97de84d3af39024886c5e
GitOfSanchita <missanchita2002@gmail.com>
2025-07-12 14:40:53 +0000

    truthy values

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -24,3 +24,25 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }                                  // Object is empty
+
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10                  // 5
+// val1 = null ?? 10               // 10
+// val1 = undefined ?? 15          // 15
+val1 = null ?? 10 ?? 20            // 10
+
+console.log(val1);           
+
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+// more than 80
